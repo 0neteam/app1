fbd3f99080b7bdf5c26e053b5328427289fdb899
teagonng <contin1984@naver.com>
2025-02-11 19:27:34 +0900

    기능추가 : 발주관리 - 발주 상세 내역 조회 , 견적서 상세 페이지 모달창 만듬 , 발주 상세 내역 검색 기능 추가 OrderContoller , OrderDAO , OrderDaoImp , OrderDTO , OrderMapper , OrderService , OrderServiceImp , orderInquiry.html 파일 수정

--- src/main/java/com/java/order/OrderContoller.java
+++ src/main/java/com/java/order/OrderContoller.java
@@ -1,19 +1,55 @@
 package com.java.order;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class OrderContoller {
+
+    private final OrderService orderService;
 
     @GetMapping("/order/orderRequest")
     public String orderRequest() {
        return "order/orderRequest";
     }
 
+    @PostMapping ("/order/orderRequest")
+    public String orderPostRequest(HttpServletRequest request) {
+       return "order/orderRequest";
+    }
+    // 발주 조회
     @GetMapping("/order/orderInquiry")
-    public String orderInquiry() {
+    public String orderInquiry(Model model) {
+        return orderService.orderInquiry(model);
+    }
+    // 발주 조회 검색
+    @GetMapping("/order/search")
+    public String searchOrders(@RequestParam(value = "searchTerm", required = false) String searchTerm , Model model) {
+        return orderService.searchOrders(model ,searchTerm);  // 검색 결과 반환
+    }
+
+    // 견적서 번호 받기
+
+
+    @GetMapping("/estimate")
+    public String handleQuoRequest(HttpServletRequest request) {
+
+        int orderId = Integer.parseInt(request.getParameter("orderId"));
+
+        System.out.println("text--------------------------------------: " + orderId);
+
+
+
         return "order/orderInquiry";
     }
+
 
     @GetMapping("/order/quotationReview")
     public String quotationReview() {
