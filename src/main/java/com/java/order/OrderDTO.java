fea526ec1405933da911da92344ae4b7a7ecdcd2
teagonng <contin1984@naver.com>
2025-02-11 19:06:59 +0900

    기능추가 : 발주관리 - 발주 상세 내역 조회 , 견적서 상세 페이지 모달창 만듬 , 발주 상세 내역 검색 기능 추가 OrderContoller , OrderDAO , OrderDaoImp , OrderDTO , OrderMapper , OrderService , OrderServiceImp , orderInquiry.html 파일 수정

---
+++ src/main/java/com/java/order/OrderDTO.java
@@ -0,0 +1,25 @@
+package com.java.order;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderDTO {
+
+    private int orderId;
+    private int orderNo;
+    private Date orderReqDate;
+    private Date orderCancelDate;
+    private String orderStatus;
+    private Date perDate;
+    private String dstn;
+    private String transpStatus;
+    private String departure;
+    private String driverName;
+}
