0cfc9d015aae0c28b747eb1a1724366b91a509a2
teagonng <contin1984@naver.com>
2025-02-12 20:09:10 +0900

    기능추가 : 발주요청 UI 수정 및 스크립트 이용 편의성 추가 , 발주요청 기능 수정중 , OrderItemDTO 추가 생성 OrderDAO , OrderDaoImp , OrderService , OrderServiceImp , OrderContoller , OrderMapper , orderRequest.html 파일 수정중

--- src/main/java/com/java/order/OrderService.java
+++ src/main/java/com/java/order/OrderService.java
@@ -7,4 +7,9 @@ public interface OrderService {
     public String orderInquiry(Model model);
     // 발주 조회 검색
     public String searchOrders(Model model ,String searchTerm);
+
+
+    String processOrderRequest(String selectedRowsJson);
+
+
 }
