5f63a3b4cddfe6bdc842009073bd2791ef26a1aa
kang S.W <swparabellum@gmail.com>
2025-02-12 14:28:13 +0900

    기능추가: list 화면 검색기능 추가중

--- src/main/java/com/java/biz/BizMapper.java
+++ src/main/java/com/java/biz/BizMapper.java
@@ -1,9 +1,6 @@
 package com.java.biz;
 
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.SelectKey;
+import org.apache.ibatis.annotations.*;
 import org.apache.ibatis.mapping.StatementType;
 
 import java.util.List;
@@ -12,10 +9,42 @@ import java.util.List;
 public interface BizMapper {
 
 
-    @Select({"<script>" +
-            "SELECT bizNo,bizName,bizType FROM stg_client" +
-            "</script>"})
+    @Select({
+            "<script>",
+            "SELECT bizNo, bizName, bizType FROM stg_client",
+            "<where>",
+            "   <choose>",
+            "       <when test='searchOption = \"1\"'>",
+            "           AND bizName LIKE CONCAT('%', #{keyword}, '%')",
+            "       </when>",
+            "       <when test='searchOption = \"2\"'>",
+            "           AND CAST(bizNo AS CHAR) LIKE CONCAT('%', #{keyword}, '%')",
+            "       </when>",
+            "       <when test='searchOption = \"3\"'>",
+            "           AND bizType LIKE CONCAT('%', #{keyword}, '%')",
+            "       </when>",
+            "   </choose>",
+            "</where>",
+            "</script>"
+    })
     public List<BizDTO> findList();
+
+//    @Select({"<script>" +
+//            "SELECT bizNo,bizName,bizType FROM stg_client" +
+//            "where true" +
+//            "<choose>" +
+//            "<when test='searchOption==\"1\"'>" +
+//            "and bizName like concat('%', #{keyword} ,'%')" +
+//            "</when>" +
+//            "<when test='searchOption==\"2\"'>" +
+//            "and bizNo like concat('%', #{keyword} ,'%')" +
+//            "</when>" +
+//            "<when test='searchOption==\"3\"'>" +
+//            "and bizType like concat('%', #{keyword} ,'%')" +
+//            "</when>" +
+//            "</choose>" +
+//            "</script>"})
+//    public List<BizDTO> findList();
 
     @SelectKey(statementType = StatementType.PREPARED, statement = "select last_insert_id() as no", keyProperty = "bizNo", before = false, resultType = int.class)
     //@Insert("INSERT INTO 'stg_client'")
